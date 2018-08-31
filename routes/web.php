34c8697b17b834c01509a4efc10dfe03b7935917
shriya01 <jain.shriya@fxbytes.com>
2018-08-31 16:37:00 +0530

    welcome page links added

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,9 @@
 |
 */
 
-
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('googlemap', 'MapController@map');
 Route::get('googlemap/direction', 'MapController@direction');
 
